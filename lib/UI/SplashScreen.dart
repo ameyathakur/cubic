1f6888e92f3807855f9a6d62a04e11eb56e5f7b8
Ameya Thakur <ameyathakur@transeunt.in>
2022-03-03 17:57:55 +0530

    03-03-2022

---
+++ lib/UI/SplashScreen.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'MainScreen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => MainScreen())));
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF1ECEC),
+        body: Center(
+          child: Image.asset('Assets/Logo.png'),
+        ),
+      ),
+    );
+  }
+}
